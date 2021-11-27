a69321cc96935bf1f9b8d22fd015d66dae4894e1
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2021-11-27 18:34:43 +0100

    Amplitud: DONE
    WIP profundidad

--- CerquesLaberint/app/src/main/java/com/example/cerqueslaberint/Cerca.java
+++ CerquesLaberint/app/src/main/java/com/example/cerqueslaberint/Cerca.java
@@ -1,7 +1,9 @@
 package com.example.cerqueslaberint;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Stack;
 
 /**
  * Created by Ramon Mas on 10/10/21.
@@ -9,9 +11,9 @@ import java.util.Collections;
  */
 
 /**
- *   AUTORS:__________________________________________
+ * AUTORS:__________________________________________
  */
-/* S'ha d'omplenar la següent taula amb els diferents valors del nodes visitats i la llargada del camí 
+/* S'ha d'omplenar la següent taula amb els diferents valors del nodes visitats i la llargada del camí
  * per les diferents grandàries de laberints proposades i comentar breument els resultats obtinguts.
  ****************************************************************************************************************
  *                  Profunditat           Amplada          Manhattan         Euclidiana         Viatjant        *
@@ -30,10 +32,9 @@ import java.util.Collections;
  *
  */
 
-public class Cerca
-{
+public class Cerca {
     static final public int MANHATTAN = 2;
-    static final public int EUCLIDEA  = 3;
+    static final public int EUCLIDEA = 3;
 
     Laberint laberint;      // laberint on es cerca
     int files, columnes;    // files i columnes del laberint
@@ -48,75 +49,159 @@ public class Cerca
      * Printea un NODO
      * !! LA X ES LA ALTURA !!
      */
-    public String print_punt(Punt punto){
+    public String print_punt(Punt punto) {
         return "Punto\tx:" + punto.x + "\ty:" + punto.y;
+    }
+
+    /*
+     * Añade al primer arraylist todos los elementos del segundo, evitando nodos repetidos
+     */
+    public void add_non_repeated(ArrayList<Punt> target, ArrayList<Punt> toAdd) {
+        for (int i = 0; i < toAdd.size(); i++) {
+            boolean found = false;
+            for (int j = 0; j < target.size(); j++) {
+                //System.out.println(print_punt(toAdd.get(i)) + "\t" + print_punt(target.get(j)));
+                if (toAdd.get(i).equals(target.get(j))) {
+                    //System.out.println("Found equal!");
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                target.add(toAdd.get(i));
+            }
+
+        }
+    }
+
+    /*
+     * Añade al Stack todos los elementos del ArrayList, evitando nodos repetidos
+     */
+    public void add_non_repeated(Stack<Punt> target, ArrayList<Punt> toAdd) {
+        ArrayList<Punt> arr_target = new ArrayList<>();
+        ArrayList<Punt> swap_order = new ArrayList<>();
+        arr_target.addAll(target);
+
+        for (int i = 0; i < toAdd.size(); i++) {
+            boolean found = false;
+            for (int j = 0; j < arr_target.size(); j++) {
+                //System.out.println(print_punt(toAdd.get(i)) + "\t" + print_punt(target.get(j)));
+                if (toAdd.get(i).equals(arr_target.get(j))) {
+                    //System.out.println("Found equal!");
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                swap_order.add(toAdd.get(i));
+            }
+        }
+
+        // Swap order to get prefered order upon pop
+        for (int i = swap_order.size()-1; i >= 0; i-- ){
+            target.push(swap_order.get(i));
+        }
     }
 
     /*
      * Dado un nodo, retorna la cantidad de nodos a los que podemos ir siguiendo un orden
      */
-    public Punt[] expand_node(Punt node){
+    public ArrayList<Punt> expand_node(Punt node) {
         ArrayList<Punt> expansion = new ArrayList<>();
-        System.out.println(print_punt(node));
         int[] orden = {laberint.ESQUERRA, laberint.AMUNT, laberint.DRETA, laberint.AVALL};
-        for (int i = 0; i < orden.length; i++){
+        for (int i = 0; i < orden.length; i++) {
 
-            if (laberint.pucAnar(node.x, node.y, orden[i])){
-                switch (orden[i]){
+            if (laberint.pucAnar(node.x, node.y, orden[i])) {
+                switch (orden[i]) {
                     // No se que es este "val" pero nunca se usa
                     case Laberint.ESQUERRA:
-                        System.out.println("puedo ir a la izquierda");
-                        expansion.add(new Punt(node.x, node.y-1, node, 0));
+                        //System.out.println("Puedo ir a la izquierda");
+                        expansion.add(new Punt(node.x, node.y - 1, node, 0));
                         break;
                     case Laberint.AMUNT:
-                        System.out.println("puedo ir arriba");
-                        expansion.add(new Punt(node.x-1, node.y, node, 0));
+                        //System.out.println("Puedo ir a la arriba");
+                        expansion.add(new Punt(node.x - 1, node.y, node, 0));
                         break;
                     case Laberint.DRETA:
-                        System.out.println("puedo ir a la derecha");
-                        expansion.add(new Punt(node.x, node.y+1, node, 0));
+                        //System.out.println("Puedo ir a la derecha");
+                        expansion.add(new Punt(node.x, node.y + 1, node, 0));
                         break;
                     case Laberint.AVALL:
-                        System.out.println("puedo ir abajo");
-                        expansion.add(new Punt(node.x+1, node.y, node, 0));
+                        //System.out.println("Puedo ir a la abajo");
+                        expansion.add(new Punt(node.x + 1, node.y, node, 0));
                         break;
                     default:
                         return null;
                 }
             }
         }
-
-        return expansion.toArray(new Punt[expansion.size()]);
+        return expansion;
+        //return expansion.toArray(new Punt[expansion.size()]);
 
     }
-    public Cami CercaEnAmplada(Punt origen, Punt desti)
-    {
-        Cami camiTrobat = new Cami(files*columnes);
+
+    public Cami CercaEnAmplada(Punt origen, Punt desti) {
+        Cami camiTrobat = new Cami(files * columnes);
         laberint.setNodes(0);
-        Punt[] expansion = expand_node(origen);
-        for (int i = 0; i < expansion.length; i++){
-            System.out.println(print_punt(expansion[i]));
+        ArrayList<Punt> closed = new ArrayList<>();
+        ArrayList<Punt> open = new ArrayList<>();
+
+        open.add(origen);
+
+        while (open.size() != 0) {
+            Punt node = open.get(0);
+            open.remove(0);
+            closed.add(node);
+            if (node.equals(desti)) {
+
+                camiTrobat.afegeix(node); // Final node
+                while (node.previ != null) { // All other nodes
+                    node = node.previ;
+                    camiTrobat.afegeix(node);
+                }
+                camiTrobat.afegeix(node); // First node
+
+
+                return camiTrobat;
+            }
+            add_non_repeated(open, expand_node(node));
+            // open.addAll(expand_node(node));
         }
-        // Implementa l'algoritme aquí
-        camiTrobat.afegeix(desti);
-        return camiTrobat;
+        return null;
     }
 
-    public Cami CercaEnProfunditat(Punt origen, Punt desti)
-    {
-        Cami camiTrobat = new Cami(files*columnes);
+    public Cami CercaEnProfunditat(Punt origen, Punt desti) {
+        Cami camiTrobat = new Cami(files * columnes);
         laberint.setNodes(0);
+        ArrayList<Punt> closed = new ArrayList<>();
+        Stack<Punt> open = new Stack<>();
 
-        // Implementa l'algoritme aquí
-        camiTrobat.afegeix(desti);
+        open.push(origen);
 
-        return camiTrobat;
+        while (!open.empty()) {
+            Punt node = open.pop();
+            closed.add(node);
+            if (node.equals(desti)) {
+
+                camiTrobat.afegeix(node); // Final node
+                while (node.previ != null) { // All other nodes
+                    node = node.previ;
+                    camiTrobat.afegeix(node);
+                }
+                camiTrobat.afegeix(node); // First node
+
+
+                return camiTrobat;
+            }
+            add_non_repeated(open, expand_node(node));
+            // open.addAll(expand_node(node));
+        }
+        return null;
     }
 
-    public Cami CercaAmbHeurística(Punt origen, Punt desti, int tipus)
-    {   // Tipus pot ser MANHATTAN o EUCLIDIA
+    public Cami CercaAmbHeurística(Punt origen, Punt desti, int tipus) {   // Tipus pot ser MANHATTAN o EUCLIDIA
         int i;
-        Cami camiTrobat = new Cami(files*columnes);
+        Cami camiTrobat = new Cami(files * columnes);
         laberint.setNodes(0);
 
         // Implementa l'algoritme aquí
@@ -126,9 +211,8 @@ public class Cerca
     }
 
 
-    public Cami CercaViatjant(Punt origen, Punt desti)
-    {
-        Cami camiTrobat = new Cami(files*columnes);
+    public Cami CercaViatjant(Punt origen, Punt desti) {
+        Cami camiTrobat = new Cami(files * columnes);
         laberint.setNodes(0);
 
         // Implementa l'algoritme aquí
