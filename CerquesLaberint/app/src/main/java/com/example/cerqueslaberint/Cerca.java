962ba7d662d16e31a00fa8460a27f173b260f27f
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2021-11-28 20:16:30 +0100

    WIP recursive TSM

--- CerquesLaberint/app/src/main/java/com/example/cerqueslaberint/Cerca.java
+++ CerquesLaberint/app/src/main/java/com/example/cerqueslaberint/Cerca.java
@@ -1,5 +1,7 @@
 package com.example.cerqueslaberint;
 
+import android.content.pm.LauncherApps;
+
 import java.util.ArrayList;
 import java.util.Stack;
 
@@ -52,6 +54,16 @@ public class Cerca {
         return "Punto\tx:" + punto.x + "\ty:" + punto.y;
     }
 
+    /**
+     * Añade todos los elementos del segundo parametro al primero. target = target U toAdd
+     * @param target
+     * @param toAdd Camino a concantenar
+     */
+    public void concat_paths(Cami target, Cami toAdd){
+        for (int i = 1; i < toAdd.longitud; i++){ // Skip first node desti = origen
+            target.afegeix(toAdd.cami[i]);
+        }
+    }
     /**
      * Devuelve la interesccion entre el primer parámetro y el segundo parámetro
      * @param target
@@ -198,14 +210,78 @@ public class Cerca {
         return camiTrobat;
     }
 
+    /**
+     * Metodo que implemtenta el problema del mercader ambulante (TSM). Mediante llamadas recursivas
+     * a la búsqueda por amplitud, este método retornará el camino más eficiente con respecto a
+     * longitud.
+     * @param from
+     * @param to
+     * @param path
+     * @param city
+     * @param min
+     * @param visited
+     * @return Camino de menor longitud entre todos las "ciudades" y la salida
+     */
+    public Cami TSM_recursive(Punt from, Punt to, Cami path, int city, int min, ArrayList<Boolean> visited){
+        Cami temp_path;
+        int current_min = min;
+        Cami recur;
+        ArrayList<Boolean> temp_visited = new ArrayList<>();
+        Cami exit = new Cami(files * columnes);
+        int nodes = laberint.nodes;
 
+        if (!visited.contains(false)){ // All visited
+            temp_path = CercaEnAmplada(from, to);
+            laberint.setNodes(nodes + laberint.nodes);
+            concat_paths(path, temp_path);
+            return path;
+        }
+
+        for (int i = 0; i < city; i++){
+            temp_visited.addAll(visited); // reset of visited values
+            nodes = laberint.nodes; // reset value of nodes
+            if (!temp_visited.get(i)){
+                temp_visited.set(i, true); // Count as visited
+                temp_path = CercaEnAmplada(from, laberint.getObjecte(i));
+                laberint.setNodes(nodes + laberint.nodes);
+                if ((path.longitud + (temp_path.longitud-1)) < current_min){
+                    concat_paths(path, temp_path);
+
+                    recur = TSM_recursive(laberint.getObjecte(i), to, path, city, current_min, temp_visited);
+                    if (recur.longitud < min){
+                        current_min = path.longitud;
+                        exit = recur;
+                    }
+                }
+            }
+        }
+
+        return exit;
+    }
+    /**
+     * Implementación del algoritmo del mercader mediante el algoritmo de busqueda de amplitud (BFS)
+     * @param origen Punto inicial
+     * @param desti Punto final
+     * @return Camino a seguir para ir desde origen hasta destino
+     */
     public Cami CercaViatjant(Punt origen, Punt desti) {
         Cami camiTrobat = new Cami(files * columnes);
         laberint.setNodes(0);
 
-        // Implementa l'algoritme aquí
-        camiTrobat.afegeix(desti);
+        // Get "city" array
+        ArrayList<Punt> city = new ArrayList<>();
+        try{
+            for (int i = 0; i < Integer.MAX_VALUE; i++){
+                city.add(laberint.getObjecte(i));
+            }
+        }catch(Exception e){
+        }
 
-        return camiTrobat;
+        ArrayList<Boolean> visited = new ArrayList<>();
+        for (int i = 0; i < city.size(); i++){ // add as much "cities" as we have
+            visited.add(false);
+        }
+
+        return TSM_recursive(origen, desti, camiTrobat, city.size(), Integer.MAX_VALUE, visited);
     }
 }
