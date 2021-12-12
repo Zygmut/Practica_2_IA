b3be19cd749ca586a45534d77f8fb60a2877dbfb
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2021-12-12 12:17:06 +0100

    Integration with heuristic
    Eculidean seems to not work

--- CerquesLaberint/app/src/main/java/com/example/cerqueslaberint/Cerca.java
+++ CerquesLaberint/app/src/main/java/com/example/cerqueslaberint/Cerca.java
@@ -47,6 +47,7 @@ public class Cerca {
 
     /**
      * Printea un NODO. Cabe decir que el valor "x" es la altura, empezando desde la celda mas alta de la izquierda
+     *
      * @param punto Punto a printear
      * @return String con los contenidos pertinentes del punto
      */
@@ -56,12 +57,13 @@ public class Cerca {
 
     /**
      * Invierte el Cami pasado por parámetro. Princio -> Final & Final -> Principio
+     *
      * @param input Camino a invertir
      * @return Camino invertido
      */
-    private Cami invert_path(Cami input){
-        Cami inverted_path = new Cami(files*columnes);
-        for (int i = input.longitud-1; i >= 0; i--){
+    private Cami invert_path(Cami input) {
+        Cami inverted_path = new Cami(files * columnes);
+        for (int i = input.longitud - 1; i >= 0; i--) {
             inverted_path.afegeix(input.cami[i]);
         }
         return inverted_path;
@@ -69,29 +71,32 @@ public class Cerca {
 
     /**
      * Concatena el primer camino con el segundo. Tiene en cuenta que el nodo final y el inicial de los caminos serán iguales
+     *
      * @param target Camino objetivo
-     * @param toAdd Camino a concantenar
+     * @param toAdd  Camino a concantenar
      * @return Camino concatenado
      */
-    private Cami concat_paths(Cami target, Cami toAdd){
+    private Cami concat_paths(Cami target, Cami toAdd) {
         Cami return_path = new Cami(files * columnes);
 
-        for (int i = 0; i < target.longitud; i++){
+        for (int i = 0; i < target.longitud; i++) {
             return_path.afegeix(target.cami[i]);
         }
-        if (target.longitud == 0){ // first iteration
-            return_path.afegeix(toAdd.cami[toAdd.longitud-1]);
+        if (target.longitud == 0) { // first iteration
+            return_path.afegeix(toAdd.cami[toAdd.longitud - 1]);
         }
 
-        for (int i = toAdd.longitud-2; i >= 0 ; i--){ // Skip first node desti = origen
+        for (int i = toAdd.longitud - 2; i >= 0; i--) { // Skip first node desti = origen
             return_path.afegeix(toAdd.cami[i]);
         }
         return return_path;
     }
+
     /**
-     * Devuelve un conjunto de datos del segundo parametro que no estan en el primer parametro  
+     * Devuelve un conjunto de datos del segundo parametro que no estan en el primer parametro
+     *
      * @param target Primer conjunto
-     * @param toAdd Segundo conjunto
+     * @param toAdd  Segundo conjunto
      * @return Conjunto de datos del segundo parametro que no estan en el primer parametro
      */
     public ArrayList<Punt> non_repeated(ArrayList<Punt> target, ArrayList<Punt> toAdd) {
@@ -112,7 +117,46 @@ public class Cerca {
     }
 
     /**
+     * Devuelve la interesccion entre el primer parámetro y el segundo parámetro
+     *
+     * @param target
+     * @param toAdd
+     * @return La interesccion entre target y toAdd
+     */
+    public ArrayList<Punt> non_repeated_heuristica(ArrayList<Punt> target, ArrayList<Punt> toAdd, int tipus, Punt desti) {
+        ArrayList<Punt> non_repeated = new ArrayList<>();
+        int j;
+        for (int i = 0; i < toAdd.size(); i++) {
+            boolean found = false;
+            for (j = 0; j < target.size(); j++) {
+                if (toAdd.get(i).equals(target.get(j))) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                Punt aux = toAdd.get(i);
+                aux.distanciaDeLinici++;
+                calculaF(aux, desti, tipus);
+                non_repeated.add(aux);
+            } else if (found) {
+                Punt auxTarget = target.get(j);
+                Punt auxToAdd = toAdd.get(i);
+                auxToAdd.distanciaDeLinici++;
+
+                if (calculaF(auxTarget, desti, tipus) > calculaF(auxToAdd, desti, tipus)) {
+                    target.remove(j);
+                    target.add(auxToAdd);
+                }
+            }
+        }
+        return non_repeated;
+    }
+
+    /**
      * Dado un punto, retorna los puntos a los que podemos ir siguiendo un orden predeterminado
+     *
      * @param node Punto de origen a expandir
      * @return Conjunto de todos los nodos a los que se puede ir
      */
@@ -144,9 +188,70 @@ public class Cerca {
     }
 
     /**
+     * Método que retorna el Menor punto de un arrayList
+     *
+     * @param open  ArrayList a inspeccionar para encontra el menor punto
+     * @param desti Desti del laberinto
+     * @param tipus Manhattan / euclidea
+     * @return Min punt en términos de f(n) = g(n) + h(n)
+     */
+    private Punt getLeast(ArrayList open, Punt desti, int tipus) {
+        Punt min = (Punt) open.get(0);
+
+        // loop to find minimum from ArrayList
+        for (int i = 1; i < open.size(); i++) {
+            if (calculaF((Punt) open.get(i), desti, tipus) < calculaF(min, desti, tipus)) {
+                min = (Punt) open.get(i);
+            }
+        }
+        return min;
+    }
+
+    /**
+     * Cálcula f(n) t.q f(n) = g(n) + h(n)
+     *
+     * @param origen Punto de origen
+     * @param desti  Punto de destino
+     * @param tipus  Manhattan / euclidea
+     * @return f(n)
+     */
+    private double calculaF(Punt origen, Punt desti, int tipus) {
+        if (tipus == 2) {
+            origen.distanciaAlFinal = calculaManhattan(origen, desti);
+        } else {
+            origen.distanciaAlFinal = calculaEuclidea(origen, desti);
+        }
+
+        return origen.distanciaAlFinal + origen.distanciaDeLinici;
+    }
+
+    /**
+     * Cálcula h(n) con fórmula Manhattan
+     *
+     * @param origen Punto de origen
+     * @param desti  Punto de desti
+     * @return distancia según fórmula
+     */
+    private int calculaManhattan(Punt origen, Punt desti) {
+        return Math.abs(origen.x - desti.x) + Math.abs(origen.y - desti.y);
+    }
+
+    /**
+     * Cálcula h(n) con fórmula Euclidea
+     *
+     * @param origen Punto de origen
+     * @param desti  Punto de desti
+     * @return distancia según fórmula
+     */
+    private double calculaEuclidea(Punt origen, Punt desti) {
+        return Math.sqrt(Math.pow(origen.x - desti.x, 2) + Math.pow(origen.y - desti.y, 2));
+    }
+
+    /**
      * Implementación del algoritmo de busqueda de ampladitud (BFS)
+     *
      * @param origen Punto inicial
-     * @param desti Punto final
+     * @param desti  Punto final
      * @return Camino a seguir para ir desde origen hasta destino
      */
     public Cami CercaEnAmplada(Punt origen, Punt desti) {
@@ -179,8 +284,9 @@ public class Cerca {
 
     /**
      * Implementación del algoritmo de busqueda de profunidad (DFS)
+     *
      * @param origen Punto inicial
-     * @param desti Punto final
+     * @param desti  Punto final
      * @return Camino a seguir para ir desde origen hasta destino
      */
     public Cami CercaEnProfunditat(Punt origen, Punt desti) {
@@ -209,20 +315,73 @@ public class Cerca {
             non_repeated = non_repeated(closed, expand_node(node));
 
             // Swap order to get prefered order upon pop
-            for (int i = non_repeated.size()-1; i >= 0; i-- ){
+            for (int i = non_repeated.size() - 1; i >= 0; i--) {
                 open.push(non_repeated.get(i));
             }
         }
         return null; // Error
     }
 
+    /**
+     * Implementación del algoritmo de busqueda de profunidad (DFS)
+     *
+     * @param origen Punto inicial
+     * @param desti  Punto final
+     * @return Camino a seguir para ir desde origen hasta destino
+     */
     public Cami CercaAmbHeurística(Punt origen, Punt desti, int tipus) {   // Tipus pot ser MANHATTAN o EUCLIDIA
-        int i;
+
+        ArrayList<Punt> closed = new ArrayList<>();
+        ArrayList<Punt> open = new ArrayList<>();
+
         Cami camiTrobat = new Cami(files * columnes);
         laberint.setNodes(0);
+        boolean found = false;
+        Punt current_punt = origen;
 
-        // Implementa l'algoritme aquí
-        camiTrobat.afegeix(desti);
+        current_punt.distanciaDeLinici = 0;
+        calculaF(current_punt, desti, tipus);
+
+        open.add(current_punt);
+
+        while (open.size() != 0) {
+            found = false;
+            Punt node = getLeast(open, desti, tipus); // Point with least f(n)
+            open.remove(0);
+
+            int i;
+            for (i = 0; i < closed.size(); i++) {
+                if (closed.get(i).equals(node)) {
+                    found = true;
+                    break;
+                }
+            }
+
+            // Repeated point
+            if (found) {
+                // Leave lest f(n)
+                if (calculaF(closed.get(i), desti, tipus) > calculaF(node, desti, tipus)) {
+                    closed.remove(i);
+                    closed.add(node);
+                }
+
+            } else {
+                closed.add(node);
+            }
+
+            if (node.equals(desti)) {
+
+                camiTrobat.afegeix(node); // Final node
+                while (node.previ != null) { // All other nodes
+                    node = node.previ;
+                    camiTrobat.afegeix(node);
+                }
+
+                laberint.setNodes(closed.size()); // Set node count
+                break;
+            }
+            open.addAll(non_repeated_heuristica(open, expand_node(node), tipus, desti));
+        }
 
         return camiTrobat;
     }
@@ -231,14 +390,15 @@ public class Cerca {
      * Metodo que implemtenta el problema del mercader ambulante (TSM). Mediante llamadas recursivas
      * a la búsqueda por amplitud, este método retornará el camino más eficiente con respecto a
      * longitud.
-     * @param from Punto origen
-     * @param to Punto final
-     * @param path Camino parcial
-     * @param min Minimo actual
+     *
+     * @param from    Punto origen
+     * @param to      Punto final
+     * @param path    Camino parcial
+     * @param min     Minimo actual
      * @param visited Conjunto de "ciudades" visitadas
      * @return Camino de menor longitud entre todos las "ciudades" y la salida
      */
-    public Cami TSM_recursive(Punt from, Punt to, Cami path, int min, ArrayList<Boolean> visited){
+    public Cami TSM_recursive(Punt from, Punt to, Cami path, int min, ArrayList<Boolean> visited) {
         Cami temp_path;
         int current_min = min;
         Cami recur;
@@ -246,26 +406,26 @@ public class Cerca {
         ArrayList<Boolean> temp_visited;
         int nodes = laberint.nodes;
 
-        if (!visited.contains(false)){ // All visited
+        if (!visited.contains(false)) { // All visited
             temp_path = CercaEnAmplada(from, to);
             laberint.setNodes(nodes + laberint.nodes);
             return concat_paths(path, temp_path);
         }
 
-        for (int i = 0; i < visited.size(); i++){
+        for (int i = 0; i < visited.size(); i++) {
             temp_visited = new ArrayList<>();
             temp_visited.addAll(visited); // reset of visited values
             nodes = laberint.nodes; // reset value of nodes
 
-            if (!temp_visited.get(i)){
+            if (!temp_visited.get(i)) {
                 temp_visited.set(i, true); // Count as visited
                 temp_path = CercaEnAmplada(from, laberint.getObjecte(i));
                 laberint.setNodes(nodes + laberint.nodes);
 
-                if ((path.longitud + (temp_path.longitud-1)) < current_min){
+                if ((path.longitud + (temp_path.longitud - 1)) < current_min) {
                     recur = TSM_recursive(laberint.getObjecte(i), to, concat_paths(path, temp_path), current_min, temp_visited);
 
-                    if ((recur.longitud < current_min) && (recur.longitud != 0)){
+                    if ((recur.longitud < current_min) && (recur.longitud != 0)) {
                         current_min = recur.longitud;
                         exit = recur;
                     }
@@ -275,10 +435,12 @@ public class Cerca {
 
         return exit;
     }
+
     /**
      * Implementación del algoritmo del mercader mediante el algoritmo de busqueda de amplitud (BFS)
+     *
      * @param origen Punto inicial
-     * @param desti Punto final
+     * @param desti  Punto final
      * @return Camino a seguir para ir desde origen hasta destino
      */
     public Cami CercaViatjant(Punt origen, Punt desti) {
@@ -287,15 +449,15 @@ public class Cerca {
 
         // Get "city" array
         ArrayList<Punt> city = new ArrayList<>();
-        try{
-            for (int i = 0; i < Integer.MAX_VALUE; i++){
+        try {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
                 city.add(laberint.getObjecte(i));
             }
-        }catch(Exception e){
+        } catch (Exception e) {
         }
 
         ArrayList<Boolean> visited = new ArrayList<>();
-        for (int i = 0; i < city.size(); i++){ // add as much "cities" as we have
+        for (int i = 0; i < city.size(); i++) { // add as much "cities" as we have
             visited.add(false);
         }
 
